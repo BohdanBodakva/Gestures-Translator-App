899c2bb01c7be3d731105f9f40bb81ffdb3d1199
TopWork0ut <olegiksen@gmail.com>
2022-05-09 15:03:44 +0300

    added block with text and modified layout

--- app/src/main/java/com/example/getsures_translator_app/MainActivity.java
+++ app/src/main/java/com/example/getsures_translator_app/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.getsures_translator_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +19,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Button btn = findViewById(R.id.first_button);
+//       jkjjjfdk
     }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (getCurrentFocus() != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+
+
+
+
 }
