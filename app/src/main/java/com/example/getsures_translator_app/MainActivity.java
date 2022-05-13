5c4694e63400a319de048476eba325d8f079cb8a
Svyatoslav Tsar <79832144+Svts3@users.noreply.github.com>
2022-05-14 00:44:01 +0300

    added settings activity

--- app/src/main/java/com/example/getsures_translator_app/MainActivity.java
+++ app/src/main/java/com/example/getsures_translator_app/MainActivity.java
@@ -4,13 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button settingsButton;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        settingsButton = (Button)findViewById(R.id.SettingsButton);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
